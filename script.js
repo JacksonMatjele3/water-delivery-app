0bb3532bfa5c96329eb9247e5e7fc4c3e6f7cfc8
Morebudi Jackson Matjele <papikimatjele@gmail.com>
2025-07-04 12:20:24 +0200

    fetched info form

---
+++ script.js
@@ -0,0 +1,5 @@
+const locationInput = document.getElementById("location");
+const dateInput = document.getElementById("date");
+const timeInput = document.getElementById("time");
+
+document.getElementById("deliveryForm").addEventListener("submit", function(){alert("You submited")});
