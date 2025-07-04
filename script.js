28e6246af590625ef87416710c34a92599e541de
Morebudi Jackson Matjele <papikimatjele@gmail.com>
2025-07-04 12:56:06 +0200

    config the button function

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const locationInput = document.getElementById("location");
 const dateInput = document.getElementById("date");
 const timeInput = document.getElementById("time");
 
-document.getElementById("deliveryForm").addEventListener("submit", function(){alert("You submited")});
+document.addEventListener("DOMContentLoaded", function(){document.getElementById("deliveryForm").addEventListener("submit", function(e){e.preventDefault(); alert("You submited");});});
+    
