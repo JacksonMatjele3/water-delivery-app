3464fc8ca778dcf39d0e27d63465f89a182ed977
Morebudi Jackson Matjele <papikimatjele@gmail.com>
2025-07-06 16:11:20 +0200

    created an object to hold input values

--- script.js
+++ script.js
@@ -2,5 +2,18 @@ const locationInput = document.getElementById("location");
 const dateInput = document.getElementById("date");
 const timeInput = document.getElementById("time");
 
-document.addEventListener("DOMContentLoaded", function(){document.getElementById("deliveryForm").addEventListener("submit", function(e){e.preventDefault(); alert("You submited");});});
+document.addEventListener("DOMContentLoaded", function(){
+        document.getElementById("deliveryForm").addEventListener("submit", function(e){e.preventDefault(); alert("You submited");
+
+    const data =
+    {
+        location: locationInput.value,
+        date: dateInput.value,
+        time: dateInput.value,
+        phone: "+27679590274"
+    };
+    
+    console.log(data);
+        });
+    });
     
